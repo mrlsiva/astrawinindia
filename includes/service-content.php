fe32bd2c18bc9028759adcc56ee10495af4fa43d
siva <pmrlsivas@gmail.com>
2025-10-31 11:03:21 +0530

    service page update

--- includes/service-content.php
+++ includes/service-content.php
@@ -1,99 +1,78 @@
-    <!-- Services -->
-    <section class="service-section position-relative">
-        <div class="container">
-            <figure class="service-image mb-0">
-                <img src="./assets/images/service-image.png" class="img-fluid" alt="">
-            </figure>
-            <div class="row">
-                <div class="col-lg-6 col-md-12 col-sm-12 col-12">
-                    <div class="service_content" data-aos="fade-right">
-                        <h5>Services We Provide</h5>
-                        <h2>Delivering Intelligent Solutions with Precision and Purpose</h2>
-                        <p class="text-size-18">At Astrawin India Pvt. Ltd., we empower enterprises with cutting-edge technology services designed to enhance efficiency, strengthen security, and drive digital growth.Our focus is on delivering exceptional quality and flawless execution, ensuring every solution adds real value to your business.</p><p class="text-size-18">Through innovation, expertise, and a commitment to excellence, we help organizations transform today’s challenges into tomorrow’s opportunities.</p>
-                        <a class="read_more text-decoration-none" href="./service.html">Read More<i
-                                class="circle fa-regular fa-angle-right"></i></a>
-                    </div>
-                </div>
-                <div class="col-lg-6 col-md-12 col-sm-12 col-12">
-                    <div class="service_contentbox">
-                        <div class="row">
-                            <div class="col-lg-6 col-md-6 col-sm-6 col-12">
-                                <div class="service-box box-mb">
-                                    <figure class="service-reboticon">
-                                        <img src="./assets/images/service-reboticon.png" alt="" class="img-fluid">
-                                    </figure>
-                                    <h4>Software Solutions</h4>
-                                    <p class="text-size-16 mb-2">Delivering customized, scalable, and high-performance software applications powered by AI and automation, enabling businesses to innovate, streamline operations, and drive digital growth
-                                    </p>
-                                    <!-- <a class="read_more text-decoration-none" href="./service.html">Read More</a> -->
-                                </div>
-                            </div>
-                            <div class="col-lg-6 col-md-6 col-sm-6 col-12">
-                                <div class="box-top">
-                                    <div class="service-box box-mb">
-                                        <figure class="service-learningicon">
-                                            <img src="./assets/images/service-learningicon.png" alt=""
-                                                class="img-fluid">
-                                        </figure>
-                                        <h4>IoT Systems</h4>
-                                        <p class="text-size-16 mb-2">Designing intelligent IoT ecosystems that connect devices, capture real-time data, and generate actionable insights through AI-driven analytics — for smarter, more efficient decision-making.</p>
-                                        <!-- <a class="read_more text-decoration-none" href="./service.html">Read More</a> -->
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-6 col-md-6 col-sm-6 col-12">
-                                <div class="service-box">
-                                    <figure class="service-scienceicon">
-                                        <img src="./assets/images/service-metalicon.png" alt="" class="img-fluid">
-                                    </figure>
-                                    <h4>Cybersecurity Services</h4>
-                                    <p class="text-size-16 mb-2">Safeguarding digital assets with advanced threat intelligence, proactive monitoring, and compliance-driven protection strategies to ensure complete cyber resilience.
-                                    </p>
-                                    <!-- <a class="read_more text-decoration-none" href="./service.html">Read More</a> -->
-                                </div>
-                            </div>
-                            <div class="col-lg-6 col-md-6 col-sm-6 col-12">
-                                <div class="box-top">
-                                    <div class="service-box">
-                                        <figure class="service-analysicon">
-                                            <img src="./assets/images/service-analysicon.png" alt="" class="img-fluid">
-                                        </figure>
-                                        <h4>Managed IT Services</h4>
-                                        <p class="text-size-16 mb-2">Providing end-to-end IT management — from infrastructure monitoring to helpdesk support — ensuring reliability, continuity, and operational excellence.</p>
-                                        <!-- <a class="read_more text-decoration-none" href="./service.html">Read More</a> -->
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-6 col-md-6 col-sm-6 col-12">
-                                <div class="service-box">
-                                    <figure class="service-scienceicon">
-                                        <img src="./assets/images/service-scienceicon.png" alt="" class="img-fluid">
-                                    </figure>
-                                    <h4>Infrastructure Management</h4>
-                                    <p class="text-size-16 mb-2">Building and maintaining robust IT infrastructures that support scalability, security, and seamless business performance across environments.
-                                    </p>
-                                    <!-- <a class="read_more text-decoration-none" href="./service.html">Read More</a> -->
-                                </div>
-                            </div>
-                            <div class="col-lg-6 col-md-6 col-sm-6 col-12">
-                                <div class="box-top">
-                                    <div class="service-box">
-                                        <figure class="service-analysicon">
-                                            <img src="./assets/images/counter-image1.png" alt="" class="img-fluid">
-                                        </figure>
-                                        <h4>Next-Generation Workspace Technologies</h4>
-                                        <p class="text-size-16 mb-2">Transforming workplaces through cloud, collaboration, and automation tools that drive productivity, mobility, and smarter employee experiences.</p>
-                                        <!-- <a class="read_more text-decoration-none" href="./service.html">Read More</a> -->
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+<div class="service_contentbox">
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+            <div class="service-box box-mb">
+                <figure class="service-reboticon">
+                    <img src="./assets/images/service-reboticon.png" alt="" class="img-fluid">
+                </figure>
+                <h4>Software Solutions</h4>
+                <p class="text-size-16 mb-2">Delivering customized, scalable, and high-performance software applications powered by AI and automation, enabling businesses to innovate, streamline operations, and drive digital growth
+                </p>
+                <a class="read_more text-decoration-none" href="./service-details.php">Read More</a>
+            </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+            <div class="box-top">
+                <div class="service-box box-mb">
+                    <figure class="service-learningicon">
+                        <img src="./assets/images/service-learningicon.png" alt=""
+                            class="img-fluid">
+                    </figure>
+                    <h4>IoT Systems</h4>
+                    <p class="text-size-16 mb-2">Designing intelligent IoT ecosystems that connect devices, capture real-time data, and generate actionable insights through AI-driven analytics — for smarter, more efficient decision-making.</p>
+                <a class="read_more text-decoration-none" href="./service-details.php">Read More</a>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+            <div class="service-box">
+                <figure class="service-scienceicon">
+                    <img src="./assets/images/service-metalicon.png" alt="" class="img-fluid">
+                </figure>
+                <h4>Cybersecurity Services</h4>
+                <p class="text-size-16 mb-2">Safeguarding digital assets with advanced threat intelligence, proactive monitoring, and compliance-driven protection strategies to ensure complete cyber resilience.
+                </p>
+                <a class="read_more text-decoration-none" href="./service-details.php">Read More</a>
+            </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+            <div class="box-top">
+                <div class="service-box">
+                    <figure class="service-analysicon">
+                        <img src="./assets/images/service-analysicon.png" alt="" class="img-fluid">
+                    </figure>
+                    <h4>Managed IT Services</h4>
+                    <p class="text-size-16 mb-2">Providing end-to-end IT management — from infrastructure monitoring to helpdesk support — ensuring reliability, continuity, and operational excellence.</p>
+                <a class="read_more text-decoration-none" href="./service-details.php">Read More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+            <div class="service-box">
+                <figure class="service-scienceicon">
+                    <img src="./assets/images/service-scienceicon.png" alt="" class="img-fluid">
+                </figure>
+                <h4>Infrastructure Management</h4>
+                <p class="text-size-16 mb-2">Building and maintaining robust IT infrastructures that support scalability, security, and seamless business performance across environments.
+                </p>
+                <a class="read_more text-decoration-none" href="./service-details.php">Read More</a>
+            </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+            <div class="box-top">
+                <div class="service-box">
+                    <figure class="service-analysicon">
+                        <img src="./assets/images/counter-image1.png" alt="" class="img-fluid">
+                    </figure>
+                    <h4>Next-Generation Workspace Technologies</h4>
+                    <p class="text-size-16 mb-2">Transforming workplaces through cloud, collaboration, and automation tools that drive productivity, mobility, and smarter employee experiences.</p>
+                <a class="read_more text-decoration-none" href="./service-details.php">Read More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
--- services.php
+++ services.php
@@ -40,7 +40,27 @@
         </figure>
     </section>
 </div>
-<?php include 'includes/service-content.php'; ?>
+<section class="service-section position-relative">
+    <div class="container">
+        <figure class="service-image mb-0">
+            <img src="./assets/images/service-image.png" class="img-fluid" alt="">
+        </figure>
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-12">
+                <div class="provide_content" data-aos="fade-right">
+                    <h5>Services We Provide</h5>
+                    <h2>Delivering Intelligent Solutions with Precision and Purpose</h2>
+                    <p class="text-size-18">At Astrawin India Pvt. Ltd., we empower enterprises with cutting-edge technology services designed to enhance efficiency, strengthen security, and drive digital growth.Our focus is on delivering exceptional quality and flawless execution, ensuring every solution adds real value to your business.</p><p class="text-size-18">Through innovation, expertise, and a commitment to excellence, we help organizations transform today’s challenges into tomorrow’s opportunities.</p>
+                    
+                </div>
+            </div>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-12">
+                <?php include 'includes/service-content.php'; ?>
+                
+            </div>
+        </div>
+    </div>
+</section>
 
 
 <?php include 'includes/footer.html'; ?>
